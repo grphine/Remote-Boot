3197c0c1c7929358436376bfa45e1ef75c1d22f1
Jay <grphine@gmail.com>
2021-10-06 23:03:43 +0100

    update express middleware

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 //SECTION: imports
 const express = require("express");
 const app = express();
+const createError = require("http-errors");
+const history = require('connect-history-api-fallback')
+
 const io = require('./io')
+
 
 app.use(express.json());
 app.use('/io', io)
+app.use(history())
 app.use('/', express.static(__dirname + '/site'))
 
 app.listen(1233, ()=>{
     console.log('server start on port 1233')
 })
+
+app.use((req, res, next) => {
+    next(createError(404));
+});
+
+app.use((req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    res.status(err.status || 500);
+    res.render("error");
+});
 
 module.exports = app;
