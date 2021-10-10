dd50c7f09ee5fcefaed26c24ad0d393030399479
Jay <grphine@gmail.com>
2021-10-10 09:39:04 +0100

    status tracking framework

--- io.js
+++ io.js
@@ -3,8 +3,16 @@ const router = express.Router()
 
 module.exports = router;
 
-router.get("/on", (req, res)=>{
-    console.log('on')
-    res.send('on')
+router.get("status", (req, res)=>{
+    console.log('status requested')
+    res.send(
+        {
+            status: 'current status',
+            ip: 'ip addr'
+        })
 })
 
+router.post('pwrBtn', (req, res)=>{
+    console.log('power button pushed')
+    res.send('pwr button push')
+})
